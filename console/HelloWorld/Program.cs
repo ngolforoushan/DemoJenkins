5bfa55556ade42e60d7d775015045d4466aa3d3f
Navid Golforosuahn <ngolforoushan@gmail.com>
2018-09-03 13:42:31 -0700

    Fix

--- console/HelloWorld/Program.cs
+++ console/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(CreateWelcomeMessage());
+            Console.WriteLine(CreateWelcomeMssage());
         }
 
         public static string CreateWelcomeMssage() => "Hello World! --NG";
